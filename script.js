9cc9b96191809a4c1e4f7d4673d99735d606828e
AKX-M <128284168+AKX-M@users.noreply.github.com>
2025-09-04 16:28:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Массив с именами файлов фотографий
     const photoFilenames = [
-        'images/Su33.png',
+        'Su33.png',
         'photo2.jpg',
         'photo3.jpg',
         'photo4.jpg',
