d64596aa45fbd57ce594d3205e52dfaa0d5f2eef
AKX-M <128284168+AKX-M@users.noreply.github.com>
2026-06-14 21:08:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Массив с именами файлов фотографий
     const photoFilenames = [
         'Su33.png',
-        'Spyral.png',
-        'Spyral_s.png',
-        'Su33_Mig31.png',
-        'Su33_Mig31_1.png',
-        'Su33_Mig31_2.png',
         'Mig23mld_Mig21smt.png',
         'MiG-23-preview.jpg',
         'MiG-23.jpg',
